41185e6078c182b8263c3f59ab7b851e7d148470
Veronika4Donchenko4 <donchenko.veronika96@gmail.com>
2023-12-14 23:47:39 +0100

    test

--- src/components/costsfilter/CostsFilter.jsx
+++ src/components/costsfilter/CostsFilter.jsx
@@ -10,10 +10,10 @@ return (
         <div className={styles['costs-filter__control']}>
         <label>Filter by year</label>
         <select value={props.year} onChange={yearChangeHandler}>
+            <option value="2023">2024</option>
             <option value="2023">2023</option>
             <option value="2023">2022</option>
             <option value="2023">2021</option>
-            <option value="2023">2020</option>
         </select>
         </div>
     </div>
